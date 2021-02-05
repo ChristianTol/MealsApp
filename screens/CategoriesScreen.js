2dd6b37b528cbc92c310973fccd76526c8847d58
ChristianTol <christian.tol1998@hotmail.com>
2021-02-05 22:59:35 +0100

    Added components and styling for all screens and added nav tabs at the bottom om the screen.

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { 
+  View, 
+  Text, 
+  FlatList, 
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
+  const renderGridItem = (itemData) => {
+    return (
+      <CategoryGridTile 
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
+          props.navigation.navigate({
+            routeName: 'CategoryMeals',
+            params: {
+              categoryId: itemData.item.id
+            }
+          });
+        }}
+      />
+    );
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-    </View>
+    <FlatList
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   );
 };
 
